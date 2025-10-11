853b565e6f506be1ec0570ae79df9ea357c50c7d
Cédric Belin <cedx@outlook.com>
2025-10-11 23:55:07 +0200

    Enable the CI

---
+++ test/SafeTransformer.Tests.ps1
@@ -0,0 +1,28 @@
+using module ../src/SafeTransformer.psm1
+
+<#
+.SYNOPSIS
+	Tests the features of the `SafeTransformer` class.
+#>
+Describe "SafeTransformer" {
+	Describe "Transform()" {
+		BeforeAll { $transformer = [SafeTransformer]::new() }
+		AfterAll { $transformer.Dispose() }
+
+		It "should remove the inline comments" {
+			$transformer.Transform("res/Sample.php") | Should -BeLikeExactly "*<`?= 'Hello World2!' `?>*"
+		}
+
+		It "remove the multi-line comments" {
+			$transformer.Transform("res/Sample.php") | Should -BeLikeExactly "*namespace dummy; class Dummy*"
+		}
+
+		It "should remove the single-line comments" {
+			$transformer.Transform("res/Sample.php") | Should -BeLikeExactly '*$className = get_class($this); return $className;*'
+		}
+
+		It "should remove the whitespace" {
+			$transformer.Transform("res/Sample.php") | Should -BeLikeExactly '*__construct() { $this->property*'
+		}
+	}
+}
--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,4 @@
 Write-Output "Performing the static analysis of source code..."
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
 Invoke-ScriptAnalyzer src -Recurse
-Invoke-ScriptAnalyzer test -Recurse
+Invoke-ScriptAnalyzer test -ExcludeRule PSUseDeclaredVarsMoreThanAssignments -Recurse
