36eb7b2e916aac1ace5355d22171fadc4a4c40b2
Cédric Belin <cedx@outlook.com>
2025-10-11 15:34:01 +0200

    Add the test fixture

---
+++ res/Sample.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+/**
+ * Implementation of the `dummy\Dummy` class.
+ */
+namespace dummy;
+
+/**
+ * A dummy class.
+ */
+class Dummy {
+
+	/**
+	 * A dummy property.
+	 * @var int
+	 */
+	public readonly int $property;
+
+	/**
+	 * Creates a new instance.
+	 */
+	public function __construct() {
+		$this->property = 123;
+	}
+
+	/**
+	 * A dummy method.
+	 * @return string The class name.
+	 */
+	public function __toString(): string {
+		// A comment.
+		$className = get_class($this);
+
+		// Another comment.
+		return $className;
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Tests</title>
+	</head>
+
+	<body>
+		<h1>
+			<?= /* An inline comment. */ 'Hello World!' ?>
+		</h1>
+	</body>
+</html>
