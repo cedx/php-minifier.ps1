c045f518f57a373aa29ccc69ad7bfa68a7ca32a9
Cédric Belin <cedx@outlook.com>
2025-10-13 18:43:28 +0200

    Update the build system

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,5 @@
 Write-Output "Performing the static analysis of source code..."
+Import-Module PSScriptAnalyzer -Scope Local
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
 Invoke-ScriptAnalyzer src -Recurse
 Invoke-ScriptAnalyzer test -ExcludeRule PSUseDeclaredVarsMoreThanAssignments -Recurse
--- tool/Test.ps1
+++ tool/Test.ps1
@@ -1,2 +1,3 @@
 Write-Output "Running the test suite..."
+Import-Module Pester -Scope Local
 Invoke-Pester test
