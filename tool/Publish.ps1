77cd592b261b7f7ffe27e56eca191b5b1cf1fd55
Cédric Belin <cedx@outlook.com>
2025-10-23 18:41:41 +0200

    Update the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,6 +1,17 @@
 . $PSScriptRoot/Default.ps1
 
 "Publishing the package..."
-$version = (Import-PowerShellDataFile "PhpMinifier.psd1").ModuleVersion
+$module = Get-Item "PhpMinifier.psd1"
+$version = (Import-PowerShellDataFile $module).ModuleVersion
 git tag "v$version"
 git push origin "v$version"
+
+$output = "var/package"
+New-Item $output -ItemType Directory
+Copy-Item $module $output
+Copy-Item *.md $output
+Copy-Item src $output -Recurse
+Copy-Item www $output -Recurse
+
+Compress-PSResource $output var
+Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath "var/$($module.BaseName).$version.nupkg"
