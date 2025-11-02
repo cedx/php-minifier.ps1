2df773a745eb5fa78637e054565fe45318d69949
Cédric Belin <cedx@outlook.com>
2025-11-02 22:32:49 +0100

    Update the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,13 +1,13 @@
 . "$PSScriptRoot/Default.ps1"
 
-"Publishing the package..."
+"Publishing the module..."
 $module = Get-Item "PhpMinifier.psd1"
 $version = (Import-PowerShellDataFile $module).ModuleVersion
 git tag "v$version"
 git push origin "v$version"
 
-$output = "var/Package"
-New-Item $output -ItemType Directory
+$output = "var/$($module.BaseName)"
+New-Item $output -ItemType Directory | Out-Null
 Copy-Item $module $output
 Copy-Item *.md $output
 Copy-Item src $output -Recurse
