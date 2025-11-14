683d8a85ab15d439977d39e993f1eab1c700e963
Cédric Belin <cedx@outlook.com>
2025-11-14 09:49:30 +0100

    Update the documentation [skip ci]

--- src/TransformMode.psm1
+++ src/TransformMode.psm1
@@ -3,6 +3,16 @@
 	The operation mode of the minifier.
 #>
 enum TransformMode {
+
+	<#
+	.SYNOPSIS
+		Applies a fast transformation.
+	#>
 	Fast
+
+	<#
+	.SYNOPSIS
+		Applies a safe transformation.
+	#>
 	Safe
 }
