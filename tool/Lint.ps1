c013f58bbe2a00d965480e82791693ba515e3b16
Cédric Belin <cedx@outlook.com>
2025-11-22 23:57:01 +0100

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -3,4 +3,4 @@ Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
 Invoke-ScriptAnalyzer src -Recurse
 Invoke-ScriptAnalyzer test -Recurse
-Test-ModuleManifest "$PSScriptRoot/../PhpMinifier.psd1" | Out-Null
+Test-ModuleManifest PhpMinifier.psd1 | Out-Null
